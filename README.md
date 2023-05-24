f39ceeff6f7e92a940d0c0aca09f465e2f8da556
Meisam Tarabkhah <m.tarabkhah@gmail.com>
2023-05-24 11:57:24 +0100

    Added GUI and new features

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # English Pronunciation Practice Assistant
 
-This Python script is designed to help you improve your English pronunciation. It works by displaying lines of text from a file and prompting you to read them out loud. The script then uses speech recognition technology to transcribe your spoken words and compare them with the original text. If you mispronounce any words, the script will give a score to your pronunciation and then display the word and provide the correct pronunciation.
+This Python script is designed to help you improve your English pronunciation. It works by displaying lines of text from a file or user input and prompting you to listen or read them out loud. The script uses speech recognition technology to transcribe your spoken words and compares them with the original text. If you mispronounce any words, the script provides correct pronunciation and gives a score to your pronunciation.
 
 ## Requirements
 
@@ -8,6 +8,7 @@ This Python script is designed to help you improve your English pronunciation. I
 - PyAudio
 - SpeechRecognition
 - pyttsx3
+- gTTS
 
 ## Installation
 
@@ -16,17 +17,33 @@ This Python script is designed to help you improve your English pronunciation. I
 
 ## Usage
 
-1. Place the text file you want to practice with in the same directory as the script and name it as `SourceText.txt`.
-2. Run the script by executing `python english_pronunciation_practice.py` in your terminal.
-3. Follow the prompts to read each line of text and receive feedback on your pronunciation.
-4. The script will provide a score for your pronunciation at the end of each line. The score is calculated as the percentage of correctly pronounced words out of the total number of words in the line.
+1. Put the text you want to practice in a text file and save it on your systems, preferably within the same directory as the script.
+2. Run the script by executing `python english_pronunciation_practice.py` in your terminal or run the GUI version by executing `python english_pronunciation_practice_gui.py`.
+3. Provide the path to the file using the "Select File" button or you can manually enter the path in the "Source File" input field in the GUI.
+4. Choose your preferred settings and select the pronunciation practice level:
+   - **Level 1**: The text will be read line by line for you, and you don't have to read or repeat.
+   - **Level 2**: The text will be displayed line by line, and you have to read it. The script provides a score for your pronunciation at the end of each line. Mispronounced words will be highlighted in the "Source Text" field and played for you.
+   - **Level 3**: Similar to Level 2, but you have to read and repeat the mispronounced words until you pronounce them correctly.
+   - **Level 4**: Similar to Level 2, but you have to read the entire text again until you pronounce it correctly.
+5. The script provides a score for your pronunciation based on the percentage of correctly pronounced words out of the total number of words in each line.
+6. You can choose between two options for text-to-speech conversion:
+   - **Offline Player**: Uses the "pyttsx3" library and offers four different player speed options: "Very Slow", "Slow", "Normal", and "Fast".
+   - **Online Player**: Uses the "gTTS" library and provides two player speed options: "Slow" and "Normal". Additionally, you can choose from four different accents: British, American, Irish, and Indian.
+7. You can adjust the player speed and accent settings in the GUI. Use the "Test Voice" button to listen to a sample text and change the settings if needed.
+8. The "Clear Form" button resets the GUI and clears all the texts and selections in the widgets.
+9. The "Select File" button opens a file dialog for you to choose a text file to practice with. Alternatively, you can manually type or paste the file path into the "Source File" input field.
+10. Hover your mouse over the radio buttons in the "Choose your player" and "Choose desired assist" sections to view helpful tips for each option.
 
-## Tips
+## Ideas for Improvement
 
-- Make sure you are in a quiet environment and speak clearly into your microphone.
-- Adjust the value in `duration = 2` for the `adjust_for_ambient_noise` in the script to optimize speech recognition for your microphone and voice.
-- You can modify the `SourceText.txt` text file to practice with different materials.
+- Add "Pause" and "Stop" options to give more control to the user during the pronunciation practice.
+- Implement checks for the existence of a text file in the provided path before attempting to read it.
+- Allow the user to type or paste text directly into the "Source Text" field for practice, instead of relying only on file input.
 
 ## Contributing
 
-If you find any bugs or have suggestions for new features, please submit an issue or pull request on GitHub.
+If you find any bugs or have suggestions for
+
+ new features, please submit an issue or pull request on GitHub.
+
+Please feel free to make any further modifications to the README file based on your specific implementation details and preferences.
--- english_pronunciation_practice.py
+++ english_pronunciation_practice.py
@@ -1,19 +1,63 @@
 import speech_recognition as sr
+# Text-To-Speech: offline version
 import pyttsx3
+# Text-To-Speech: online version
+from gtts import gTTS
+from playsound import playsound
+import os
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter.tix import * # for Balloon
+
 
 listener = sr.Recognizer()
 speaker = pyttsx3.init()
 voices = speaker.getProperty('voices')
 speaker.setProperty('voice',voices[1].id)
 
+# Changing the text of a entry widget to a new text
+def set_text(e,text):
+    e.delete(1.0,END)  # Deleting previous text
+    e.insert(1.0,text) # Setting new text
+    # '1.0' is the position of the beginning of the text; This is the same as index '0' for Entry widgets
+    # Other Example: '2.3' is the position before the fourth character of the second line.
+    root.update() # While updating the text widgets in a loop, the window is not getting updated until the loop is running. 
+                  # So, you won't see anything in the Tk window until the loop has completed.
+                  # To avoid it 'freezing', you should update it periodically.
+
+def set_label(e,text):
+    e['text'] = text
+    root.update()
 
 
-def talk(text):
-    print(text)
+def talk_online(e,text,accent,speed):
+    set_text(e,text)
+    tts = gTTS(text, lang='en', tld=accent, slow=speed)
+    tts.save('tempvoice.mp3')
+    playsound("tempvoice.mp3")
+    os.remove("tempvoice.mp3")
+    
+
+def talk_offline(e,text,rate):
+    set_text(e,text)
+    speaker.setProperty("rate", rate)
     speaker.say(text)
     speaker.runAndWait()
     # with sr.Microphone() as source:
     #     listener.adjust_for_ambient_noise(source, duration = 1)
+
+def talk(e,text):
+    if TTSvalue.get() == 0:
+        RATE = [100,150,200,250]
+        talk_offline(e,text,RATE[speedvalue.get()])
+    else:
+        if speedvalue.get() == 1:
+            slow = True
+        else:
+            slow = False
+
+        ACCENTDICT = ['co.uk','us','ie','co.in']
+        talk_online(e,text,ACCENTDICT[accentvalue.get()],slow)
 
 def compare(text1, text2):
     text1 = text1.lower().split()
@@ -38,32 +82,252 @@ def assist(text):
     flag = True
     try:
         with sr.Microphone() as source:
-            print(text)
+            set_text(sourceText,text)
             listener.adjust_for_ambient_noise(source, duration = 2)
-            print('Listening...')
+            set_label(informationLabel, 'Listening...')
             voicecommand = listener.listen(source)
             textcommand = listener.recognize_google(voicecommand)
+            # textcommand = input("test!!! type instead of saying:")
             # if 'stop' in textcommand.lower():
             #     return False
-            talk(textcommand)
+            talk(recognizedText,textcommand)
+            score, diff = compare(text,textcommand)
+            if score<100:
+                set_label(informationLabel,f'{round(score,2)}% accurate\nThis is the words you had missed')
+                for word in diff:
+                    talk(sourceText,word)
     except:
         pass
-    return textcommand
+    return score, diff
 
-file1 = open('SourceText.txt', 'r', encoding="utf8")
-lines = file1.readlines()
-for text1 in lines:
-    if text1.strip() != '':
-        text2 = assist(text1)
-        score , diff = compare(text1,text2)
-        if score<100:
-            print(f'{round(score,2)}% accurate\nThese are the words you had missed')
-            for word in diff:
-                talk(word)
-        else:
-            print('Well done!')
+# Clearing all the user input values
+def clearButtonClick():
+    set_text(sourceText,"")
+    set_text(recognizedText,"")
+    set_text(fileText,"")
+    set_label(informationLabel,"")
+    TTSvalue.set(0)
+    speedvalue.set(0)
+    accentvalue.set(0)
+    assistvalue.set(0)
+    veryslowradiobutton.configure(state = NORMAL)
+    fastradiobutton.configure(state = NORMAL)
+    Britishradiobutton.configure(state = DISABLED)
+    Americanradiobutton.configure(state = DISABLED)
+    Irishradiobutton.configure(state = DISABLED)
+    Indianradiobutton.configure(state = DISABLED)
+
+def startButtonClick():
+    assistchoice = assistvalue.get()+1
+    fileadd = fileText.get("1.0",END).strip('\n')
+    if fileadd == '' : # Other conditions could be added
+                       # like Checking the existance of a file
+        set_label(informationLabel, "You should select the source file first")
+        return
+    file1 = open(fileadd, 'r', encoding="utf8")
+    lines = file1.readlines()
+    for text1 in lines:
+        if text1.strip() != '':
+            if assistchoice == 1:
+                talk(sourceText,text1)
+            else:
+                score1, diff1 = assist(text1)                
+                if score1<100:
+                    if assistchoice == 3:
+                        set_label(informationLabel, "Let's practice what you have miss-pronounced:")
+                        for word in diff1:
+                            score2 = score1
+                            while score2 < 100:
+                                score2, diff2 = assist(word)
+                    elif assistchoice == 4:
+                        set_label(informationLabel, "Let's try again:")
+                        while score1<100:
+                            score1, diff1 = assist(text1)
+                else:
+                    set_label(informationLabel,'Well done!')
+    file1.close()
+
+def TestButtonClick():
+    talk(sourceText,"This is a sample text")
+
+def fileButtonClick():
+    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    set_text(fileText,filename)
 
 
-# flag = False
-# while flag:
-#     flag = assist()
+def TTSselectionchange():
+    if TTSvalue.get() == 1: # In case of online player "gTTS"
+                            # The accent options are available so their radiobuttons should be ativated
+                            # But, the only "Slow" and "Normal" speeds are available
+                            # So, the other speed options should be deactivated
+                            # Also, if "Very Slow" option was active, we will change it to "Slow"
+                            # Similarly, if "Fast" option was active, we will change it to "Normal"
+        veryslowradiobutton.configure(state = DISABLED)
+        fastradiobutton.configure(state = DISABLED)
+        Britishradiobutton.configure(state = NORMAL)
+        Americanradiobutton.configure(state = NORMAL)
+        Irishradiobutton.configure(state = NORMAL)
+        Indianradiobutton.configure(state = NORMAL)
+        if speedvalue.get() == 0:
+            speedvalue.set(1)
+        elif speedvalue.get() == 3:
+            speedvalue.set(2)
+    else:                   # In case of off-line player "pyttsx3"
+                            # The accent options are not available so their radiobuttons should be deativated
+                            # But, all the speeds are available
+                            # So, "Very Slow" and "Fast" speed options should be activated
+        veryslowradiobutton.configure(state = NORMAL)
+        fastradiobutton.configure(state = NORMAL)
+        Britishradiobutton.configure(state = DISABLED)
+        Americanradiobutton.configure(state = DISABLED)
+        Irishradiobutton.configure(state = DISABLED)
+        Indianradiobutton.configure(state = DISABLED)
+
+root = Tk()
+root.title("English Pronounciation Practice")
+root.geometry("650x300")
+
+#Create a tooltip
+tip = Balloon(root)
+
+# Creating Lable Widgets
+sourceLabel = Label(root,text="Source Text")
+sourceLabel.place(x=5,y=5)
+RecognizedLabel = Label(root,text="Recognized Text")
+RecognizedLabel.place(x=5,y=60)
+Label(root, text="Choose your player:", justify = LEFT).place(x=5,y=115)      # If you do not need to access/update any widgets later, you can skip assigning a variable name to it
+Label(root, text="Choose player's speed:", justify = LEFT).place(x=5,y=140)
+Label(root, text="Choose player's accent:", justify = LEFT).place(x=5,y=165)
+Label(root, text="Choose desired assist:", justify = LEFT).place(x=5,y=190)
+Label(root, text="Source File", justify = LEFT).place(x=5,y=215)
+informationLabel = Label(root,text="",width=57,fg='red', borderwidth=2, relief="groove")
+informationLabel.place(x=120,y=260)
+
+# Creating Button Widgets
+startButton = Button(root,text="start",command=startButtonClick,width=10)
+startButton.place(x=550,y=5)
+testButton = Button(root,text="Test Voice",command=TestButtonClick,width=10)
+testButton.place(x=550,y=40)
+clearButton = Button(root,text="Clear Form",command=clearButtonClick,width=10)
+clearButton.place(x=550,y=75)
+fileButton = Button(root,text="Select File",command=fileButtonClick,width=10)
+fileButton.place(x=550,y=215)
+
+# Creating Text Widgets
+sourceText = Text(root,width=50,height = 3)
+sourceText.place(x=120,y=5)
+recognizedText = Text(root,width=50,height = 3)#,state=DISABLED)    # If you want to make the text widget, "read only"
+                                                                    # Before and after inserting, change the state, otherwise it won't update
+                                                                    # text.configure(state='normal')
+                                                                    # text.insert('end', 'Some Text')
+                                                                    # text.configure(state='disabled')
+recognizedText.place(x=120,y=60)
+fileText = Text(root,width=50,height = 1)
+fileText.place(x=120,y=215)
+
+# Creating RadioButton Widgets
+TTSvalue = IntVar()
+offlineradiobutton = Radiobutton(root, text="Offline", variable=TTSvalue, value=0,command=TTSselectionchange)
+offlineradiobutton.place(x=140,y=115)
+tip.bind_widget(offlineradiobutton,balloonmsg='Uses "pyttsx3" for Text to Speech conversion, You do not need to be connected to the internet')
+onlineradiobutton = Radiobutton(root, text="Online", variable=TTSvalue, value=1,command=TTSselectionchange)
+onlineradiobutton.place(x=220,y=115)
+tip.bind_widget(onlineradiobutton,balloonmsg='Uses "gTTS" for Text to Speech conversion, You need to be connected to the internet')
+
+speedvalue = IntVar()
+veryslowradiobutton = Radiobutton(root, text="Very Slow", variable=speedvalue, value=0)
+veryslowradiobutton.place(x=140,y=140)
+slowradiobutton = Radiobutton(root, text="Slow", variable=speedvalue, value=1)
+slowradiobutton.place(x=220,y=140)
+normalradiobutton = Radiobutton(root, text="Normal", variable=speedvalue, value=2)
+normalradiobutton.place(x=300,y=140)
+fastradiobutton = Radiobutton(root, text="Fast", variable=speedvalue, value=3)
+fastradiobutton.place(x=380,y=140)
+
+accentvalue = IntVar()
+Britishradiobutton = Radiobutton(root, text="British", variable=accentvalue, value=0,state= DISABLED)
+Britishradiobutton.place(x=140,y=165)
+Americanradiobutton = Radiobutton(root, text="American", variable=accentvalue, value=1,state= DISABLED)
+Americanradiobutton.place(x=220,y=165)
+Irishradiobutton = Radiobutton(root, text="Irish", variable=accentvalue, value=2,state= DISABLED)
+Irishradiobutton.place(x=300,y=165)
+Indianradiobutton = Radiobutton(root, text="Indian", variable=accentvalue, value=3,state= DISABLED)
+Indianradiobutton.place(x=380,y=165)
+
+assistvalue = IntVar()
+Level1Radiobutton = Radiobutton(root, text="Level 1", variable=assistvalue, value=0)
+Level1Radiobutton.place(x=140,y=190)
+tip.bind_widget(Level1Radiobutton,balloonmsg="Assistant will read the whole text for you")
+Level2Radiobutton = Radiobutton(root, text="Level 2", variable=assistvalue, value=1)
+Level2Radiobutton.place(x=220,y=190)
+tip.bind_widget(Level2Radiobutton,balloonmsg='''Assistant will show the text line by line for you to read,
+then it shows the recognized text from your voice,
+as well as the words you have mispronounced''')
+Level3Radiobutton = Radiobutton(root, text="Level 3", variable=assistvalue, value=2)
+Level3Radiobutton.place(x=300,y=190)
+tip.bind_widget(Level3Radiobutton,balloonmsg='''Assistant will show the text line by line for you to read,
+then it shows the recognized text from your voice,
+as well as the words you have mispronounced.
+It Asks you to repeat the mispronounced words until you pronounce them all correctly''')
+Level4Radiobutton = Radiobutton(root, text="Level 4", variable=assistvalue, value=3)
+Level4Radiobutton.place(x=380,y=190)
+tip.bind_widget(Level4Radiobutton,balloonmsg='''Assistant will show the text line by line for you to read,
+then it shows the recognized text from your voice,
+as well as the words you have mispronounced.
+It Asks you to read the text again until you pronounce it correctly''')
+
+# Creating Ckeckbutton Widgets
+# v5 = IntVar()
+# v5.set(1)
+# Checkbutton(root, text="Level 1", variable=v5).place(x=110,y=215)
+
+root.mainloop()
+
+
+
+# def setspeed():
+#     print("How fast do you want to hear the voice")
+#     speed = {1: ['Very Slow', 100],
+#              2: ['Slow', 150],
+#              3: ['Normal', 200],
+#              4: ['Fast', 250]}
+#     for key,value in speed.items():
+#         print(f"{key}. {value[0]} -> Listen to sample voice")
+#         speaker.setProperty("rate", value[1])
+#         talk(sourceText,"This is a sample text")
+        
+#     speedchoice = int(input(f"Enter your choice (1~{key})"))
+#     rate = speed[speedchoice][1]
+#     return rate
+
+# def setaccent():
+#     # 'en-us': 'English (US)', 'en-ca': 'English (Canada)', 'en-uk': 'English (UK)', 'en-gb': 'English (UK)', 
+#     # 'en-au': 'English (Australia)', 'en-gh': 'English (Ghana)', 'en-in': 'English (India)', 
+#     # 'en-ie': 'English (Ireland)', 'en-nz': 'English (New Zealand)', 'en-ng': 'English (Nigeria)', 
+#     # 'en-ph': 'English (Philippines)', 'en-za': 'English (South Africa)', 'en-tz': 'English (Tanzania)'
+#     accentdict = {'1': ['UK', 'co.uk'],
+#                   '2': ['US', 'us'],
+#                   '3': ['Irish', 'ie'],
+#                   '4': ['Indian', 'co.in']}
+#     print("Which accent do you want to hear the voice")
+#     for key,value in accentdict.items():
+#         print(f"{key}. {value[0]} -> Listen to sample voice")
+#         talk_online(f"{value[0]} accent: 'I often visit the ancient castle to admire its grandeur and pass through its imposing gates.'",value[1])
+#     accentchoice = input(f"Enter your choice (1~{key})")
+#     return accentdict[accentchoice][1]
+
+
+# accent = setaccent()
+
+# rate = setspeed()
+
+
+# print("Available assist methods:")
+# print("1. Listen to the whole text")
+# print('''2. Try ro read each line of the text to check your pronounciation
+#     and see what you have miss-pronounced''')
+# print('''3. Try ro read each line of the text to check your pronounciation
+#     and then try to correct what you have miss-pronounced''')
+# print('''4. Try ro read each line of the text to check your pronounciation
+#     and then try untill you have no have miss-pronounciation''')
+# assistchoice = input("Select the assist you require (1~4): ")
